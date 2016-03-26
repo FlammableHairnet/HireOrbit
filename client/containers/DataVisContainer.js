d6d6763f641467dccc5fe7cf606ffa91f32bbb62
Daniel Kim <dkim2707@gmail.com>
2016-03-26 01:03:19 -0700

    dataViz d3 query barchart basic function

--- client/containers/DataVisContainer.js
+++ client/containers/DataVisContainer.js
@@ -4,27 +4,34 @@ import dataVisual from '../reducers/index';
 import d3 from 'd3';
 import _ from 'lodash';
 
-  const spec = {
-    h: 600,
-    w: 300
-  }
-
   const dataset = [ 5, 10, 15, 20, 25 ];
+
   const dataset2 = [
-  {x: 'a', y: 20},
-  {x: 'b', y: 14},
-  {x: 'c', y: 12},
-  {x: 'd', y: 19},
-  {x: 'e', y: 18},
-  {x: 'f', y: 15},
-  {x: 'g', y: 10},
-  {x: 'h', y: 14}
-];
+    {x: 'a', y: 20},
+    {x: 'b', y: 14},
+    {x: 'c', y: 12},
+    {x: 'd', y: 19},
+    {x: 'e', y: 18},
+    {x: 'f', y: 15},
+    {x: 'g', y: 10},
+    {x: 'h', y: 14}
+  ];
+
+  const SetIntervalMixin = {
+    componentWillMount: function() {
+      this.intervals = [];
+    },
+    setInterval: function() {
+      this.intervals.push(setInterval.apply(null, arguments));
+    },
+    componentWillUnmount: function() {
+      this.intervals.map(clearInterval);
+    }
+  };
 
 
   let Chart = React.createClass({
     render: function () {
-      console.log('chart this.props.children ', this.props.children)
       return (
         <svg width={this.props.width}
              height={this.props.height} >
@@ -42,32 +49,62 @@ import _ from 'lodash';
     },
 
     render: function () {
-      let props = this.props;
-      let data = props.data.map(function (d) {return d.y})
-
+      let props = this.props
+      console.log('PRRROOOPSS', props)
       let xScale = d3.scale.ordinal()
-                     .domain(d3.range(data.length))
-                     .rangeRoundBands([0, this.props.width, 0.05])
-
+                     .domain(d3.range(props.data.length))
+                     .rangeRoundBands([0, props.width, 0.05])
       let yScale = d3.scale.linear()
-                     .domain( [ 0, d3.max(data)] )
-                     .range([0, this.props.height])
+                     .domain( [ 0, d3.max(props.data)] )
+                     .range([0, props.height])
 
-      console.log('xScale.rangeBand() ', xScale.rangeBand())
-      let Bars = data.map(function(e, i) {
+      let bars = props.data.map(function(e, i) {
+        console.log('asfeeee',e)
         var height = yScale(e),
-            y = props.height - height,
             width = xScale.rangeBand(),
+            y = props.height - height,
             x = xScale(i)
-            console.log('bars', height, y, width, x);
+
+        return (
+          <Rect
+            width={width}
+            height={height}
+            x={x}
+            y={y}
+          />
+        )
       });
 
       return (
-        <g>{Bars}</g>
+        <g>{bars}</g>
       )
     }
-
   });
+
+  var Rect = React.createClass({
+    mixins: [SetIntervalMixin],
+    getDefaultProps: function () {
+      return {
+        width: 0,
+        height: 0,
+        x: 0,
+        y: 0
+      }
+    },
+
+    render: function () {
+      console.log('this !', this.props)
+      return (
+        <rect
+          className="bar"
+          height={this.props.height}
+          width={this.props.width}
+          x={this.props.x}
+          y={this.props.y}
+        ></rect>
+      )
+    }
+  })
 
 
 
@@ -81,15 +118,15 @@ class DataVisContainer extends Component {
     const location = data.location;
     const totalResults = data.totalResults;
     const query = data.query;
-
   }
+
 
 
   render () {
     console.log('adsfa',this.props.dataVisual)
     return (
-      <Chart width={spec.w} height={spec.h}>
-        <Bar data={dataset2} width={spec.w} height={spec.h} />
+      <Chart width={500} height={500}>
+        <Bar data={this.props.dataVisual} width={500} height={500} />
       </Chart>
     )
   }
