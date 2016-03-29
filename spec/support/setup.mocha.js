46464a7c1d8c5474af27f9730cc4ccaf4ba601cd
Daniel Kim <dkim2707@gmail.com>
2016-03-28 18:39:33 -0700

    Add comments about functionality

--- spec/support/setup.mocha.js
+++ spec/support/setup.mocha.js
@@ -16,21 +16,33 @@ global.navigator = global.window.navigator;
 const $ = jquery(global.window);
 
 // build 'renderComponent' helper that should render a given react class
+  // three parameters: Component, props placed ComponentClass and application level state to be injected into store.
 function renderComponent(ComponentClass, props, state) {
+  // Make an instance of ComponentClass, renderIntoDocument renders component
+    // into detached DOM in document, requires a dom given by jsdom.
+    // Provide store with initial state, and reducers
+    // Instead of props={props}, use spread operator to prevent this.props.props
   const componentInstance = TestUtils.renderIntoDocument(
     <Provider store={createStore(reducers, state)}>
       <ComponentClass {...props} />
     </Provider>
   );
-
+  // Working with react and dom, wrap with jquery call to access matchers from chai-jquery
   return $(ReactDOM.findDOMNode(componentInstance)); // produces HTML
 }
 
-// Build helper for simulating events
+
+// Build helper for simulating events, creating an extension of jQuery.
+  // any new instantiation of will have access to the jquery prototype
 $.fn.simulate = function(eventName, value) {
+  // if value passed in
   if (value) {
+    // this html elemented selected, this.val will set value input
     this.val(value);
   }
+  // .simulate; This is possibly the single most useful utility in ReactTestUtils.
+    // Example: var node = this.refs.button; ReactTestUtils.Simulate.click(node);
+      // Example: $('div').simulate('change', 'new comment')
   TestUtils.Simulate[eventName](this[0]);
 }
 
