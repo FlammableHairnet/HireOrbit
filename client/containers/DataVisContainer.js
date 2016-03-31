80da70b3ea3920d007d789f2a9649c0845ab93d4
Daniel Kim <dkim2707@gmail.com>
2016-03-30 18:32:27 -0700

    Clean up code, add comments before rebase

--- client/containers/DataVisContainer.js
+++ client/containers/DataVisContainer.js
@@ -3,7 +3,8 @@ import { connect } from 'react-redux';
 import dataVisual from '../reducers/index';
 import d3 from 'd3';
 import _ from 'lodash';
-
+  
+  // Styles
   const filler = {
     fill: 'white',
     textAnchor: 'middle'
@@ -13,20 +14,7 @@ import _ from 'lodash';
     textAnchor: 'middle'
   }
 
-  const dataset = [ 5, 10, 15, 20, 25 ];
-
-  const dataset2 = [
-    {x: 'a', y: 20},
-    {x: 'b', y: 14},
-    {x: 'c', y: 12},
-    {x: 'd', y: 19},
-    {x: 'e', y: 18},
-    {x: 'f', y: 15},
-    {x: 'g', y: 10},
-    {x: 'h', y: 14}
-  ];
-
-  // Cross-cutting-concerns
+  // Cross-cutting-concerns, MIXINS
   const SetIntervalMixin = {
     componentWillMount: function() {
       this.intervals = [];
@@ -39,7 +27,7 @@ import _ from 'lodash';
     }
   };
 
-  // Create Chart component to return svg
+  // CHART COMPONENT returns svg tags
   let Chart = React.createClass({
     render: function () {
       return (
@@ -52,7 +40,7 @@ import _ from 'lodash';
   });
 
 
-  //Bar components returns content of Rect component
+  // BAR COMPONENT with scaling functionality
   let Bar = React.createClass({
     getDefaultProps: function() {
       return {
@@ -61,38 +49,52 @@ import _ from 'lodash';
     },
 
     render: function () {
-      let props = this.props
-      // Padding of 25 percent of max value
-      let padding = d3.max(props.data) * 0.25;
-      // Create a xScale to scale width inputs
+      // has data, width, and height
+      let props = this.props;
+      // array of objects, query, location, totalResults
+      let totalResult = _.map(props.data, function (obj) {
+        return obj.totalResults;
+      });
+      // obtain the locations of the search
+      let locations = _.map(props.data, function (obj) {
+        return obj.location;
+      });
+        
+      // Initial state, data is empty array, so nothing breaks. When submit, populates the data array, let data be totalResult.
+      let data = props.data.length > 0 ? totalResult : props.data;
+      // If data is not an empty array, calculate of 25 percent of max value in array.
+      let paddingValue = data.length > 0 ? d3.max(data)*.25 : 0;
+      // Create a xScale to scale width inputs of array length
       let xScale = d3.scale.ordinal()
-                     .domain(d3.range(props.data.length))
-                     .rangeRoundBands([0, props.width], 0.4)
-
-      // padding: domain input of 0 to max, max + plus 25 percent of max value obtain in data array.
+                     .domain(d3.range(data.length))
+                     .rangeRoundBands([0, props.width], 0.6)
+      // padding: domain input of 0 to max with height padding
       let yScale = d3.scale.linear()
-                     .domain( [ 0, d3.max(props.data)+padding] )
+                     .domain( [ 0, d3.max(data) + paddingValue] )
                      .range([0, props.height])
-      // Iterate through the data array
-      let bars = props.data.map(function(dataPoint, i) {
-        // scale the height
-        var height = yScale(dataPoint),
-        // scale width using rangeBand, outputs an integer
-            width = xScale.rangeBand(),
-            // props height minus scaled height
-            y = props.height - height,
-            // scale index
-            x = xScale(i);
-        // for each iteration, send props to Rect component
+      // Will not iterate on empty array, if array is populated, it will run.
+      let bars = props.data.map(function(obj, i) {
+      // scale the height
+      let height = yScale(obj.totalResults),
+          // scale width using rangeBand, outputs an integer
+          width = xScale.rangeBand(),
+          // props height minus scaled height
+          y = props.height - height,
+          // scale index
+          x = xScale(i);
+        // For each iteration, send props to Rect component
         return (
-          <Rect
-            key={dataPoint}
-            width={width}
-            height={height}
-            x={x}
-            y={y}
-            data={dataPoint}
-          />
+          <g>
+            <Rect
+              key={obj.location}
+              width={width}
+              height={height}
+              x={x}
+              y={y}
+              data={obj.totalResults}
+              location={obj.location}
+            />
+          </g>
         )
       });
       // When bar returns, render contents of bars
@@ -100,11 +102,9 @@ import _ from 'lodash';
         <g>{bars}</g>
       )
     }
-
-
   });
 
-  //
+  //RECT COMPONENT with transition functionality
   var Rect = React.createClass({
     // use Mixins to cancel setIntervals when not needed
     mixins: [SetIntervalMixin],
@@ -116,7 +116,6 @@ import _ from 'lodash';
         y: 0
       }
     },
-
     // Set the lifecycle
     getInitialState: function () {
       return {
@@ -124,23 +123,13 @@ import _ from 'lodash';
         height: 0
       }
     },
-
-    // Should return true to avoid bugs, when return false,
-      // render() will be skipped shouldComponentUpdate, componentWillUpdate will be not be called.
-    shouldComponentUpdate: function (nextProps) {
-      return this.props.height !== this.state.height;
-    },
-    // Before render, log the message
-    componentWillMount: function () {
-      console.log('will mount');
-    },
-    // When component receives new props, not on initial render.
+    // When component receives new props, reset milliseconds back to 0.
     componentWillReceiveProps: function (nextProps) {
-      console.log('next props', nextProps)
-      this.setState({milliseconds: 0, height: this.props.height})
+      this.setState({milliseconds: 0})
     },
     // When render activates, run the setInterval with the tick function.
     componentDidMount: function () {
+      // Call enter methods here
       this.setInterval(this.tick, 10);
     },
     // Every tick, add 10 to the state.milliseconds
@@ -149,41 +138,45 @@ import _ from 'lodash';
     },
 
     render: function () {
-      var bounce = d3.ease('bounce');
-      var height = this.state.height + (this.props.height - this.state.height) * bounce(Math.min(1, this.state.milliseconds/1000));
-      var y = height + this.props.y;
+      // use the d3 bounce transition
+      var bounce = d3.ease('bounce'),
+          height = this.props.height * bounce(Math.min(1, this.state.milliseconds/800)),
+          style = height < 30 ? alt : filler;
+          
       return (
         <g>
-        <rect
-          className="bar"
-          height={height}
-          width={this.props.width}
-          x={this.props.x}
-          y={this.props.y} >
+          <rect
+            className='bar'
+            height={height}
+            width={this.props.width}
+            x={this.props.x}
+            y={this.props.y} 
+            >
           </rect>
           <text
-          x={this.props.x + (this.props.width/2)}
-          y={ height < 35 ? this.props.y : this.props.y+20}
-          style={ height < 35 ? alt : filler}
-          >
-          {this.props.data}
+            x={this.props.x + (this.props.width/2)}
+            y={ height < 30 ? this.props.y : this.props.y+20}
+            style={style}
+            >
+            {this.props.data}
+          </text>
+          <text
+            x={this.props.x + (this.props.width/2)}
+            style={{textAnchor: 'middle', textTransform: 'capitalize'}}
+            y={this.props.y-20}
+            >
+            {this.props.location}
           </text>
         </g>
       )
     }
+  });
 
-  })
-
-
-
-
-
+//DataVisContainer smart comoponent, access to Redux store.
 class DataVisContainer extends Component {
-
   constructor(props) {
     super(props)
   }
-
   // Chart returns svg, with contents of Bar components.
   render () {
     return (
@@ -193,12 +186,9 @@ class DataVisContainer extends Component {
     )
   }
 }
-
-
-
-
+//Access the state
 function mapStateToProps({dataVisual}) {
   return {dataVisual};
-}
-
-export default connect(mapStateToProps, null)(DataVisContainer)
+};
+//Convert DataViseContainer into an smart component
+export default connect(mapStateToProps, null)(DataVisContainer);
